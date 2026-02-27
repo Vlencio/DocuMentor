b41b2df2dbfdd0d421ef0d548e7f71377c650e14
Vlencio <valenciopedro15@gmail.com>
2026-02-27 13:53:26 -0300

    Test API

--- backend/rag_engine.py
+++ backend/rag_engine.py
@@ -24,7 +24,7 @@ data_dir = os.path.join(base_dir, 'data')
 
 class RagEngine:
     def __init__(self, model_name='sentence-transformers/all-MiniLM-L6-v2'):
-        self.client = Groq() #PUT YOUR GROQ API KEY HERE, OR SET IT IN THE .env FILE
+        self.client = Groq(api_key='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') #PUT YOUR GROQ API KEY HERE, OR SET IT IN THE .env FILE
         self.embeddings = HuggingFaceEmbeddings(model_name=model_name)
         self.vector_store = None
     
